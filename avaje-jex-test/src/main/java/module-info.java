afa1ee895dedba9e3f5b10d9d42a716facb06198
Rob Bygrave <robin.bygrave@gmail.com>
2022-05-19 22:33:32 +1200

    org.apiguardian.api ... needed in module-info for jex-test

--- avaje-jex-test/src/main/java/module-info.java
+++ avaje-jex-test/src/main/java/module-info.java
@@ -6,6 +6,7 @@ open module io.avaje.jex.test {
   requires transitive io.avaje.http.client;
   requires static com.fasterxml.jackson.databind;
   requires static io.avaje.inject.test;
+  requires static org.apiguardian.api; // stink man !!
 
   provides io.avaje.inject.test.Plugin with io.avaje.jex.test.JexInjectPlugin;
 }
