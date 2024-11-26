96d25fe31e22556fca6710bd5fd28cd5a6e2c847
Rob Bygrave <robin.bygrave@gmail.com>
2024-11-27 08:31:12 +1300

    Add htmx module (#84)
    Need to follow up with an integration test under examples

---
+++ avaje-jex-htmx/src/main/java/io/avaje/jex/htmx/TemplateContentCache.java
@@ -0,0 +1,30 @@
+package io.avaje.jex.htmx;
+
+import io.avaje.jex.Context;
+
+/**
+ * Defines caching of template content.
+ */
+public interface TemplateContentCache {
+
+  /**
+   * Return the key given the request.
+   */
+  String key(Context req);
+
+  /**
+   * Return the key given the request with form parameters.
+   */
+  String key(Context req, Object formParams);
+
+  /**
+   * Return the content given the key.
+   */
+  String content(String key);
+
+  /**
+   * Put the content into the cache.
+   */
+  void contentPut(String key, String content);
+
+}
---
+++ avaje-jex-htmx/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.avaje.jex.htmx {
+
+  requires transitive io.avaje.jex;
+  requires transitive io.avaje.htmx.api;
+  requires transitive jdk.httpserver;
+
+  exports io.avaje.jex.htmx;
+}
