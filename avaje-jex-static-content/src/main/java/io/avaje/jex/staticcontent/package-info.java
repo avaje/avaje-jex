88db62d5a742055f0800214833b4a585b808fa8b
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-12-03 20:01:22 -0500

    doc

--- avaje-jex-static-content/src/main/java/io/avaje/jex/staticcontent/package-info.java
+++ avaje-jex-static-content/src/main/java/io/avaje/jex/staticcontent/package-info.java
@@ -2,7 +2,7 @@
  * Static Content API - see {@link io.avaje.jex.staticcontent.StaticContentService}.
  *
  * <pre>{@code
- * var staticContent = StaticContentService.createCP().resource("/public").directoryIndex("index.html");
+ * var staticContent = StaticContentService.createCP("/public").directoryIndex("index.html");
  * final Jex.Server app = Jex.create()
  *   .routing(staticContent.createService())
  *   .port(8080)
--- avaje-jex-static-content/src/main/java/module-info.java
+++ avaje-jex-static-content/src/main/java/module-info.java
@@ -2,7 +2,7 @@
  * Defines the Static Content API for serving static resources with Jex - see {@link io.avaje.jex.staticcontent.StaticContentService}.
  *
  * <pre>{@code
- * var staticContent = StaticContentService.createCP().resource("/public").directoryIndex("index.html");
+ * var staticContent = StaticContentService.createCP("/public").directoryIndex("index.html");
  * final Jex.Server app = Jex.create()
  *   .routing(staticContent.createService())
  *   .port(8080)
