dc0e5abe06020005ee772ac7e545ab43f0ad287c
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-12-03 23:57:28 -0500

    fix test failing without jackson

--- avaje-jex-test/src/main/java/module-info.java
+++ avaje-jex-test/src/main/java/module-info.java
@@ -4,10 +4,8 @@ open module io.avaje.jex.test {
 
   requires transitive io.avaje.jex;
   requires transitive io.avaje.http.client;
-  requires static com.fasterxml.jackson.databind;
-  requires static io.avaje.jsonb;
+  requires transitive com.fasterxml.jackson.databind;
   requires static io.avaje.inject.test;
-  requires static org.apiguardian.api; // stink man !!
 
   provides io.avaje.inject.test.Plugin with io.avaje.jex.test.JexInjectPlugin;
 }
