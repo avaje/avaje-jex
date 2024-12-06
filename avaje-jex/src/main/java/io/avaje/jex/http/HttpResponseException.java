bd7883154026e6221242a8c6faa4adb8d4d6a4d8
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-12-05 19:26:58 -0500

    Update HttpResponseException.java

--- avaje-jex/src/main/java/io/avaje/jex/http/HttpResponseException.java
+++ avaje-jex/src/main/java/io/avaje/jex/http/HttpResponseException.java
@@ -10,23 +10,13 @@ import java.util.Map;
 public class HttpResponseException extends RuntimeException {
 
   private final int status;
-  private final Map<String, String> details;
-
-  public HttpResponseException(int status, String message, Map<String, String> details) {
-    super(message);
-    this.status = status;
-    this.details = details;
-  }
 
   public HttpResponseException(int status, String message) {
-    this(status, message, Collections.emptyMap());
+    super(message);
+    this.status = status;
   }
 
   public int getStatus() {
     return status;
-  }
-
-  public Map<String, String> getDetails() {
-    return details;
   }
 }
