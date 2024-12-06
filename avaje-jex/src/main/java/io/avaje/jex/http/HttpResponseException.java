5f7dc7d2b46e6d61429050f81c0127e0f933416e
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-12-05 19:35:50 -0500

    Tidy HttpResponseException (#128)
    * Update HttpResponseException.java
    
    * Update HttpResponseException.java

--- avaje-jex/src/main/java/io/avaje/jex/http/HttpResponseException.java
+++ avaje-jex/src/main/java/io/avaje/jex/http/HttpResponseException.java
@@ -1,8 +1,5 @@
 package io.avaje.jex.http;
 
-import java.util.Collections;
-import java.util.Map;
-
 /**
  * Throwing an uncaught {@code HttpResponseException} will interrupt http processing and set the
  * status code and response body with the given message
@@ -10,23 +7,13 @@ import java.util.Map;
 public class HttpResponseException extends RuntimeException {
 
   private final int status;
-  private final Map<String, String> details;
-
-  public HttpResponseException(int status, String message, Map<String, String> details) {
-    super(message);
-    this.status = status;
-    this.details = details;
-  }
 
   public HttpResponseException(int status, String message) {
-    this(status, message, Collections.emptyMap());
+    super(message);
+    this.status = status;
   }
 
   public int getStatus() {
     return status;
-  }
-
-  public Map<String, String> getDetails() {
-    return details;
   }
 }
