d1aa6d145d168ae338f464e85c19a4c2f71cf6c9
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-02-10 10:08:50 -0500

    more tests (#186)

---
+++ avaje-jex/src/test/java/io/avaje/jex/http/TrailingSlashTest.java
@@ -0,0 +1,42 @@
+package io.avaje.jex.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.http.HttpResponse;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import io.avaje.jex.Jex;
+import io.avaje.jex.core.TestPair;
+
+class TrailingSlashTest {
+
+  static TestPair pair = init();
+
+  static TestPair init() {
+    final Jex app =
+        Jex.create()
+            .config(c -> c.socketBacklog(0).ignoreTrailingSlashes(false))
+            .get("/slash", ctx -> {});
+
+    return TestPair.create(app);
+  }
+
+  @AfterAll
+  static void end() {
+    pair.shutdown();
+  }
+
+  @Test
+  void get() {
+    HttpResponse<String> res = pair.request().path("slash/").GET().asString();
+    assertThat(res.statusCode()).isEqualTo(404);
+  }
+
+  @Test
+  void getNoTrailing() {
+    HttpResponse<String> res = pair.request().path("slash").GET().asString();
+    assertThat(res.statusCode()).isEqualTo(204);
+  }
+}
