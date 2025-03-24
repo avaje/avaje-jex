fdebfcc59412c0269e81af4ad5653c7860d9ce58
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-03-23 22:46:29 -0400

    Add multivalue header methods (#234)
    * Add multivalue header methods
    
    - add multi-value header methods
    - fix compression matching
    
    * Update JsonTest.java
    
    * fix compression

--- avaje-jex/src/main/java/io/avaje/jex/core/JdkContext.java
+++ avaje-jex/src/main/java/io/avaje/jex/core/JdkContext.java
@@ -221,13 +221,17 @@ final class JdkContext implements Context {
   }
 
   private String header(Headers headers, String name) {
-    final List<String> values = headers.get(name);
-    return values == null || values.isEmpty() ? null : values.getFirst();
+    return headers.getFirst(name);
   }
 
   @Override
   public String header(String key) {
     return header(exchange.getRequestHeaders(), key);
+  }
+
+  @Override
+  public List<String> headerValues(String key) {
+    return exchange.getRequestHeaders().get(key);
   }
 
   @Override
@@ -261,8 +265,18 @@ final class JdkContext implements Context {
   }
 
   @Override
-  public Headers headers() {
+  public Headers requestHeaders() {
     return exchange.getRequestHeaders();
+  }
+
+  @Override
+  public Headers responseHeaders() {
+    return exchange.getResponseHeaders();
+  }
+
+  @Override
+  public List<String> responseHeaderValues(String key) {
+    return exchange.getResponseHeaders().get(key);
   }
 
   @Override
