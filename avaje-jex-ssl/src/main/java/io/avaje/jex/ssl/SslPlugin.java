9da774b2f4ff7a914c4425541e21b0b9e9b5bf06
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-11-10 23:20:26 -0500

    [ssl] simplify ssl plugin

--- avaje-jex-ssl/src/main/java/io/avaje/jex/ssl/SslPlugin.java
+++ avaje-jex-ssl/src/main/java/io/avaje/jex/ssl/SslPlugin.java
@@ -2,7 +2,10 @@ package io.avaje.jex.ssl;
 
 import java.util.function.Consumer;
 
+import javax.net.ssl.SSLContext;
+
 import io.avaje.jex.spi.JexPlugin;
+import io.avaje.jex.ssl.core.DSslPlugin;
 
 /**
  * Plugin that Configures Jex with SSL and mTLS.
@@ -30,4 +33,7 @@ public sealed interface SslPlugin extends JexPlugin permits DSslPlugin {
   static SslPlugin create(Consumer<SslConfig> consumer) {
     return new DSslPlugin(consumer);
   }
+
+  /** The configured SSLContext */
+  SSLContext sslContext();
 }
