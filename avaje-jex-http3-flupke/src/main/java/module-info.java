8fbc2aa7fd7b1daee73e64aca7c844b59cdc73bf
Mahied Maruf <contact@mechite.com>
2025-12-04 22:28:21 +0000

    Update module-info.java
    Fixes: #335
    Signed-off-by: Mahied Maruf <contact@mechite.com>

--- avaje-jex-http3-flupke/src/main/java/module-info.java
+++ avaje-jex-http3-flupke/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module io.avaje.jex.http3.flupke {
-
+  exports io.avaje.jex.http3.flupke;
+  
   requires transitive io.avaje.jex.ssl;
   requires transitive tech.kwik.flupke;
   requires java.base;
